ca87f057238af9c817542196a43649a57cc620d5
Tarek <tarekmsolli@gmail.com>
2023-02-28 13:26:58 +0100

    Added an auto clicker
    Added an auto clicker button that gives you an extra 5 points every 2 seconds and can be purchased for 50 points (the price is  multiplied by 2 for every time you buy it) and also added counters for the number of auto clickers added and a counter for the price, also changed the ui design to a bisque color

--- js/app.js
+++ js/app.js
@@ -1,8 +1,34 @@
 var COUNTER = document.querySelector("#counter");
 var COOKIE = document.querySelector("#cookie");
 var POINTS = 0;
+
+var AUTOCLICKER = document.querySelector("#click-auto");
+var AUTOCOUNTER = 0;
+var AUTOPRICE = 50;
+
+var STATS = document.querySelector("#stats");
+
 function addPoint(){
-    POINTS = POINTS + 1;
+    POINTS += 1;
     COUNTER.innerText = POINTS;
 }
+
+function addClicker(){
+    if (POINTS >= AUTOPRICE){
+    POINTS -= AUTOPRICE;
+    AUTOCOUNTER += 1;
+    AUTOPRICE = AUTOPRICE * 2;
+    COUNTER.innerText = POINTS;
+    STATS.innerHTML = `<p>Auto clickers: ${AUTOCOUNTER}</p> <p>Price = ${AUTOPRICE}</p>`;
+    }
+}
+
+setInterval(()=>{
+    POINTS += AUTOCOUNTER * 5;
+    COUNTER.innerText = POINTS;
+}, 2000);
+
+
+
 COOKIE.addEventListener("click", addPoint);
+AUTOCLICKER.addEventListener("click", addClicker)
